491c818911b4d1fa1e8dd4ca5cd4d534fa2aba2c
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-04-25 23:40:07 +0500

    Variables files updated successfully

--- 01_Python_For_AI/Variables_Python/Variables_Examples.py
+++ 01_Python_For_AI/Variables_Python/Variables_Examples.py
@@ -1,2 +1,38 @@
-a = "yasir"
+# Variable: Variables are the fundamental concepts in programing 
+# Variables are containers for future use
+# In python there is no need to tell the datatype of variable
+# In python, there is no variable declaration
+# In python, when we create a variable without telling its datatype is called dynamic typing
+# The opposite of dynamic typing is static typing
+# php and python have dynamic tying 
+# A variable store multiple datatype is called dynamic binding and opposite of dynamic binding is called static binding
+
+
+# Variable examples
+name = "Mirza Yasir Abdullah Baig" # No need of telling datatype - Dynamic Tying 
+print(name)
+
+# we can change the value of a variable in python
+name = 7
+print(name) # it will print 7
+
+name = 125.77
+print(name) # it will print 125.77 - Dynamic binding
+
+# Special declaration in Python
+a = 1; b = 3; c = 4
 print(a)
+print(b)
+print(c)
+
+a, b, c = 2, 5, 7
+print(a)
+print(b)
+print(c)
+
+a = b = c = 7
+print(a)
+print(b)
+print(c)
+
+# This is all about variables for now... hope so you enjoyed it
