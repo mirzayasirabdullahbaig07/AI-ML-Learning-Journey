c84d378792f016291679463cc31ee30e849ec415
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-04-25 23:23:40 +0500

    Datatype file is updated successfully

--- 01_Python_For_AI/Datatypes_Func_Python/Datatypes_Func_Python.py
+++ 01_Python_For_AI/Datatypes_Func_Python/Datatypes_Func_Python.py
@@ -1,1 +1,40 @@
-#what is datatype in python?
+# Python supports 3 main categories of data types
+# Basic datatpes: Integers, Float, String, Complex and Boolean
+# Container Types: List, Tuple, Sets and Dictonary
+# User-Defined Types: Classes
+
+# Integer - Numbers 1, 2, 4, 5 ....n
+print(7)
+# Limit of Integer in python 
+print(1e309)
+
+# Float - Decimal numbers like 0.1, 0.6, 1.55 or any
+print(4.5)
+# Limit of Float in Python
+print(1.7e308)
+
+# Boolean - True/False
+print(True)
+print(False)
+
+# Sting - "", """""", """"
+print("Mirza Yasir Abdullah Baig")
+print('Mirza Yasir Abdullah Baig')
+print("""Mirza Yasir Abdullah Baig""")
+
+# Complex Number - 3x, 4y or ... any
+print(4 + 3j)
+print(3 - 12j)
+
+
+# List - Like array - []
+print([1,2,3,4,5,6])
+
+# Tuple - Like List - ()
+print((1,2,3,4,5,6))
+
+# Sets - set union/set interaction - {}
+print({1,2,3,4,5,6})
+
+# Dictonary - key-value pairs - {}
+print({"Name": "Mirza Yasir Abdullah Baig", "Age": "24", "Education": "Bs-Computer Science"})
