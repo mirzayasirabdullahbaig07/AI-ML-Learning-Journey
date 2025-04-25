2685bedafd479e54548b8a8af9f89cffd8d48c2d
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-04-25 22:25:40 +0500

    Files Updates after Print Function Discussion

--- 01_Python_For_AI/Print_Func_Python/Print_Func_Example.py
+++ 01_Python_For_AI/Print_Func_Python/Print_Func_Example.py
@@ -1,1 +1,26 @@
-print("yasir")
+#What is print: print is the built-in-function, we use that print function to print anything
+
+#Example No:1
+print("Mirza Yasir Abdullah Baig")
+
+# You can print any type of datatype
+print(107) #integer
+print(False) #Boolean
+print(0.1) #float
+print(2 + 3j) #complex number
+
+# You can print many different things in one time
+print("India", "Pakistan", "Nepal")
+
+# You can also print different datatypes in one time
+print("Mirza Yasir Abdullah Baig", 24, 3.12, True)
+
+#sep parameter: separator
+print("Mirza", "Yasir", "Abdullah", "Baig", sep = '/')
+
+#End parameter
+print("Mirza", "Yasir", "Abdullah", end= ' ')
+print("Baig")
+
+#That's it, this is all about the print function
+#Print function is very commonly used function in python, you can use it sub-conciously in your programs
