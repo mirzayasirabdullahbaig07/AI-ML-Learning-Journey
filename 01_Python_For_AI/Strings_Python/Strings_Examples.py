37a5766935083c91aeed20826f061546e3aaa224
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-04-29 01:39:32 +0500

    Strings file Updated Successfully

---
+++ 01_Python_For_AI/Strings_Python/Strings_Examples.py
@@ -0,0 +1,18 @@
+# What is Strings? Strings are sequence of characters
+# In Python strings are sequence of unicode characters - Like Yasir - Y, A, S, I, R - These are characters 
+# Characters are converted in unicode then unicode is converted in binary and machine only understand the binary(0, 1)
+
+# 01 Creating String - How we create strings in Python?
+a = 'Mirza Yasir Abdullah Baig' # Single inverted commas
+print(a)
+b = "Mirza Yasir Abdullah Baig" # Double Inverted Commas
+print(b)
+# Why the concepts of '' and "" commas
+c = "it's raining outside" # here we use this kind of strings
+print(c)
+d = """yasir is an Ai developer and a machine learning expert"""
+print(d)
+e = '''this kind of strings are used when we write multiple lines and paragraphs best example is blogs'''
+print(e)
+f = str("Mirza Yasir Abdullah Baig")
+print(f)
