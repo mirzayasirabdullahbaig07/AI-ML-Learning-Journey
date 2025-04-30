6edbab6b16d72a019a3661ed29e019049a0bf4e8
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-04-30 23:40:27 +0500

    String file updated successfully

--- 01_Python_For_AI/13_Strings_Python/Strings_Examples.py
+++ 01_Python_For_AI/13_Strings_Python/Strings_Examples.py
@@ -19,8 +19,8 @@ f = str("Mirza Yasir Abdullah Baig")
 print(f)
 
 # 02 Accessing String - How we access the strings?
-# Concept of Indexing in String
 
+# Concept of Indexing in String
 # Examples
 name = "YASIR"
 print(name)
@@ -47,7 +47,6 @@ print (name[-5]) # Output Y
 # Slicing is another technique
 # In indexing u can only extract only one character
 # In i want a character from" mirza yasir abdullah baig" i want yasir for this i use slicing
-
 # Example for Slicing
 slicestr = "Mirza Yasir Abdullah Baig"
 print(slicestr[0:6]) # starting position and the end number + 1
@@ -75,6 +74,161 @@ del editstr
 print(editstr) # This will tell there is no editstr it means it is deleted
 del editstr[0] # This will not run as well 
 print(editstr[2])
+
+# 04 Strings Operators
+# Arthematic Operators - In strings, we only use + and * operators
+
+# String addition
+sumstr = "Yasir" + "Baig"
+print(sumstr) 
+sumstr1 = "Yasir" + " " + "Abdullah"
+print(sumstr1)
+
+# Multiplication Strings 
+multistr = "*"*40
+print(multistr) 
+multistr1 = "Yasir"*5
+print(multistr1)
+
+# Rational Operators in strings
+a1 = "yasir"
+b1 = "baig"
+print(a1==b1)
+
+c1 = "Abdullah"
+d1 = "Mirza"
+print(c1!=d1)
+
+e1 = "Islamabad"
+f1 = "kasur"
+# This comparison is done by lexiographically. In dictonary, the word which comes first is smaller, and the word which comes later is bigger.
+print(e1>f1)
+g1 = "Yasir"
+h1 = "yasir"
+print(g1>h1)
+# Small letters are also come later so they are big
+
+# Logical Operators in strings
+# "" - Empty strings in python are false
+# Non empty strings are true
+a11 = "Yasir"
+b22 = "Baig"
+print(a11 and b22)
+print(a11 or b22)
+print("" and a11)
+print("" or b22)
+
+# Loops operators in strings
+loopstr = "Mirza yasir abdullah baig"
+for i in loopstr:
+    print(i)
+# You can run all slicing on this loops operatior[0:92: or whatever]
+
+# Membership Operators In strings
+# In not in
+memstr = "Mirza Yasir Abdullah Baig"
+print('M' in memstr)
+print('M' not in memstr)
+
+# Functions in Strings
+# Common Functions which are provided for all iterables list, tuples, str, dic, and set
+# Some are only strings function
+
+# 01 Len function in string 
+str_fun = "Mirza Yasir Abdullah Baig"
+print(len(str_fun))
+# 02 Max function in string
+print(max(str_fun)) # On the basis of accsi values
+# 03 Min function in string
+print(min(str_fun)) # On the basis of accsi values
+# 04 Sorted function in string
+print(sorted(str_fun)) # Sort the characters in ascending order
+# And it will related the output in list form
+# You can also get in desending order if you want
+print(sorted(str_fun, reverse=True))
+
+# Only applicable on strings
+mixfuncstr = "Mirza Yasir adullah baig"
+# 01 capatilize/title/uppercase/lowercase
+# Capatilize the first letter of word, it wont able to change the existed string
+print(mixfuncstr.capitalize())
+# Title function always capatalize the each 1 letter of each word
+print(mixfuncstr.title())
+# Uppercase make the full string in upper. UPPER
+print(mixfuncstr.upper())
+# Lowercase make the small letter. LOWER, lower
+print(mixfuncstr.lower())
+# Swapcase - change upper to lower and lower to upper
+print(mixfuncstr.swapcase())
+# Count function - Tell the string of particular substring
+print(mixfuncstr.count("Y")) 
+# Find/index
+print(mixfuncstr.find("Y")) # It tell the first occurance of substring
+# if the substring is not available then it will give -1
+print(mixfuncstr.index("Y")) 
+print(mixfuncstr.index("LL")) # the code will crash
+# The main difference between find and index is if their is no substring available then in find it will show -1 and index show error
+# Always prefer find
+
+# endswith/startswith
+print(mixfuncstr.endswith("aig"))
+print(mixfuncstr.startswith("Mir"))
+
+#Format Function
+formatstr = "My name is {} and i am {}, my age is {}".format("Mirza Yasir Abdullah Baig", "AI/ML Engineer",24)
+
+formatstr = "My name is {0} and i am {1}, my age is {2}".format("Mirza Yasir Abdullah Baig", "AI/ML Engineer",24)
+
+formatstr = "My name is {name} and i am {status}, my age is {age}".format(name = "Mirza Yasir Abdullah Baig", status = "AI/ML Engineer",age = 24)
+
+#isalnum/isalpha/isdecimal/isdigit/isidentifier
+# Use for validation and asking questions
+isalnumstr = "yasir07"
+print(isalnumstr.isalnum()) # Output True
+isalnumstr1 = "yasir07#"
+print(isalnumstr1.isalnum()) # Output False
+
+isalphastr = "yasir"
+print(isalnumstr.isalpha()) # Output True
+isalphastr1 = "yasir07"
+print(isalnumstr1.isalpha()) # Output False
+
+numstr = 7
+print("7".isdigit()) # True
+print("7A".isdigit()) # False
+
+identifierstr = "Yasir_Baig"
+print(identifierstr.isidentifier()) # True
+identifierstr1 = "Yasir-Baig"
+print(identifierstr1.isidentifier()) # False
+
+# There are so many isfunction in string for different work 
+
+# Split function in string
+# split convert the str in list
+splitstr = "Mirza Yasir Abdullah Baig"
+print(splitstr.split()) # ['Mirza', 'Yasir', 'Abdullah', 'Baig']
+print(splitstr.split("i")) # ['M','rza', 'Yas','r', 'Abdullah', 'Ba',g']
+print(splitstr.split("x")) # ["Mirza Yasir Abdullah Baig"]
+
+# Join function in string
+# It is like reverse of split
+# it changes the list in str - use for urls
+joinstr = ['Mirza', 'Yasir', 'Abdullah', 'Baig']
+print("-".join(['Mirza', 'Yasir', 'Abdullah', 'Baig']))
+
+# Replace function in strings
+replacestr = "mirza yasir abdullah"
+print(replacestr.replace("abdullah", "baig"))
+
+# Strip - remove the spaces
+stripstr = "mirza              yasir       abdullah baig"
+print(stripstr.strip())
+
+
+
+
+
 
 
 
